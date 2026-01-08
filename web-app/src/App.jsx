13693e58b4bcf1865003edac1adfc763d1822736
Oleg <voleg239@gmail.com>
2026-01-08 13:58:12 -0500

    Add React Router and Home page

--- web-app/src/App.jsx
+++ web-app/src/App.jsx
@@ -3,6 +3,14 @@ import "./App.css";
 import { supabase } from "../supabaseClient";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import Home from "./components/Home";
+import {
+    BrowserRouter as Router,
+    Routes,
+    Route,
+    Navigate,
+} from "react-router-dom";
+
 
 function App() {
     const [session, setSession] = useState(null);
@@ -45,11 +53,21 @@ function App() {
                 {/* <Auth supabaseClient={supabase} appearance={{ theme: ThemeSupa }} />; */}
                 <button onClick={signUp}>Sign in with Google</button>
             </>
+
         );
     } else {
         return (
             <div>
                 <h2>Welcome, {session?.user?.email}</h2>
+                <Router>
+                    <Routes>
+                        <Route
+                            exact
+                            path="/"
+                            element={<Home />}
+                        />
+                    </Routes>
+                </Router>
                 <button onClick={signOut}>Sign out</button>
             </div>
         );
