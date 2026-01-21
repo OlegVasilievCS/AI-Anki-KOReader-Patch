699c61c66b0c98cef50c5f4a5bd594128d038409
Oleg <voleg239@gmail.com>
2026-01-21 13:16:12 -0500

    Add filtering to fethch only sentences that were not added yet to the Anki cards

--- web-app/src/components/SavedWords.jsx
+++ web-app/src/components/SavedWords.jsx
@@ -14,7 +14,9 @@ const SavedWords = () => {
             const { data, error } = await supabase
                 .from('anki_saved_words')
                 .select()
-                .eq('email', (await supabase.auth.getSession()).data.session.user.email);
+                .eq('email', (await supabase.auth.getSession()).data.session.user.email)
+                .eq('added_to_anki', 'False');
+
 
             if (error) {
                 setFetchError('Could not fetch the records')
